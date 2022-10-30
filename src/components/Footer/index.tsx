d22192089cc50f0483fffade6c918972863c3055
Huy Tuyen <fsoft.datnn@gmail.com>
2022-10-30 14:06:41 +0700

    Add animation for footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -18,6 +18,7 @@ import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 import { BiMailSend } from 'react-icons/bi';
 import React from 'react';
 import Logo from '../Logo';
+import FadeInUp from '../Animation/FadeInUp';
 
 const SocialButton = ({
   children,
@@ -61,71 +62,65 @@ const ListHeader = ({ children }: { children: ReactNode }) => {
 
 export default function Footer() {
   return (
-    <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}
-    >
-      <Container as={Stack} maxW={'6xl'} py={10}>
-        <SimpleGrid templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }} spacing={8}>
-          <Stack spacing={6}>
-            <HStack>
-              <Logo />
-              <Text>Thiền tôn Phật Quang</Text>
-            </HStack>
-            <Text fontSize={'sm'}>© 2022 Chakra Templates. All rights reserved</Text>
-            <Stack direction={'row'} spacing={6}>
-              <SocialButton label={'Twitter'} href={'#'}>
-                <FaTwitter />
-              </SocialButton>
-              <SocialButton label={'YouTube'} href={'#'}>
-                <FaYoutube />
-              </SocialButton>
-              <SocialButton label={'Instagram'} href={'#'}>
-                <FaInstagram />
-              </SocialButton>
+    <FadeInUp>
+      <Box
+        bg={useColorModeValue('gray.50', 'gray.900')}
+        color={useColorModeValue('gray.700', 'gray.200')}
+      >
+        <Container as={Stack} maxW={'6xl'} py={10}>
+          <SimpleGrid templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }} spacing={8}>
+            <Stack spacing={6}>
+              <HStack>
+                <Logo />
+                <Text>Thiền tôn Phật Quang</Text>
+              </HStack>
+              <Text fontSize={'sm'}>© 2022 Chakra Templates. All rights reserved</Text>
+              <Stack direction={'row'} spacing={6}>
+                <SocialButton label={'Twitter'} href={'#'}>
+                  <FaTwitter />
+                </SocialButton>
+                <SocialButton label={'YouTube'} href={'#'}>
+                  <FaYoutube />
+                </SocialButton>
+                <SocialButton label={'Instagram'} href={'#'}>
+                  <FaInstagram />
+                </SocialButton>
+              </Stack>
             </Stack>
-          </Stack>
-          <Stack align={'flex-start'}>
-            <ListHeader>Company</ListHeader>
-            <Link href={'#'}>About us</Link>
-            <Link href={'#'}>Blog</Link>
-            <Link href={'#'}>Contact us</Link>
-            <Link href={'#'}>Pricing</Link>
-            <Link href={'#'}>Testimonials</Link>
-          </Stack>
-          <Stack align={'flex-start'}>
-            <ListHeader>Support</ListHeader>
-            <Link href={'#'}>Help Center</Link>
-            <Link href={'#'}>Terms of Service</Link>
-            <Link href={'#'}>Legal</Link>
-            <Link href={'#'}>Privacy Policy</Link>
-            <Link href={'#'}>Satus</Link>
-          </Stack>
-          <Stack align={'flex-start'}>
-            <ListHeader>Stay up to date</ListHeader>
-            <Stack direction={'row'}>
-              <Input
-                focusBorderColor={'ttpq.400'}
-                placeholder={'Your email address'}
-                bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-                border={0}
-                _focus={{
-                  bg: 'whiteAlpha.300',
-                }}
-              />
-              <IconButton
-                //   bg={useColorModeValue('green.400', 'green.800')}
-                //   color={useColorModeValue('white', 'gray.800')}
-                //   _hover={{
-                //     bg: 'green.600',
-                //   }}
-                aria-label='Subscribe'
-                icon={<BiMailSend />}
-              />
+            <Stack align={'flex-start'}>
+              <ListHeader>Company</ListHeader>
+              <Link href={'#'}>About us</Link>
+              <Link href={'#'}>Blog</Link>
+              <Link href={'#'}>Contact us</Link>
+              <Link href={'#'}>Pricing</Link>
+              <Link href={'#'}>Testimonials</Link>
             </Stack>
-          </Stack>
-        </SimpleGrid>
-      </Container>
-    </Box>
+            <Stack align={'flex-start'}>
+              <ListHeader>Support</ListHeader>
+              <Link href={'#'}>Help Center</Link>
+              <Link href={'#'}>Terms of Service</Link>
+              <Link href={'#'}>Legal</Link>
+              <Link href={'#'}>Privacy Policy</Link>
+              <Link href={'#'}>Satus</Link>
+            </Stack>
+            <Stack align={'flex-start'}>
+              <ListHeader>Stay up to date</ListHeader>
+              <Stack direction={'row'}>
+                <Input
+                  focusBorderColor={'ttpq.400'}
+                  placeholder={'Your email address'}
+                  bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+                  border={0}
+                  _focus={{
+                    bg: 'whiteAlpha.300',
+                  }}
+                />
+                <IconButton aria-label='Subscribe' icon={<BiMailSend />} />
+              </Stack>
+            </Stack>
+          </SimpleGrid>
+        </Container>
+      </Box>
+    </FadeInUp>
   );
 }
