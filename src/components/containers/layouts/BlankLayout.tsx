64f5565d32ada6bca31c0e7cad8cdc512ec6425b
Huy Tuyen <fsoft.datnn@gmail.com>
2022-10-30 14:18:16 +0700

    fix tsc

--- src/components/containers/layouts/BlankLayout.tsx
+++ src/components/containers/layouts/BlankLayout.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
-const BlankLayout = ({ children: Children }: { children: ReactNode }) => {
+const BlankLayout = ({ children: Children }: { children: React.ReactNode }) => {
   return <Box>{Children}</Box>;
 };
 
