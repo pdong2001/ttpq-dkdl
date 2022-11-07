2cb39b1254029080b147e06294e780c7cfe40228
Huy Tuyen <fsoft.datnn@gmail.com>
2022-11-07 17:47:05 +0700

    update guideline use common api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,10 @@ import MultiStepRegister from '../MultiStepRegister';
 import Timeline from '../Timeline';
 import API from '~/apis/constants';
 import useAxios from '~/hooks/useAxios';
+import { useAppDispatch, useAppSelector } from '~/hooks/reduxHook';
+import { useEffect } from 'react';
+import { login } from '~/apis/auth/slice';
+import useAPIData from '~/hooks/useAPIData';
 export default function Home() {
   const { data, error, loading } = useAxios(
     {
@@ -16,11 +20,27 @@ export default function Home() {
     (response) => response.data,
   );
   if (!loading) {
-    console.log('test common api', data, error);
+    console.log('use axios', data, error);
   }
 
+  const dispatch = useAppDispatch();
+  const auth = useAppSelector((state) => state.auth);
+  useEffect(() => {
+    dispatch(login({ username: 'ToolDKDL', password: 'ToolDKDL@1231@' }));
+  }, []);
+  useAPIData(auth, {
+    onFullfilled: (data1) => {
+      console.log('success with redux', data1);
+    },
+    onRejected: (error1) => {
+      console.log('error with redux', error1);
+    },
+    onPending: () => {
+      console.log('loading...');
+    },
+  });
   return (
-    <Box position={'relative'} w='full' minW={'sm'}>
+    <Box position={'relative'} w='full'>
       <MultiStepRegister />
       <GreatCeremonyInfo />
       <DepartmentInfos />
