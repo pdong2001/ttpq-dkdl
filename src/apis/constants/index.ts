9023264ea9d54e34646f217c6622630dbe5b77a5
Huy Tuyen <fsoft.datnn@gmail.com>
2022-11-08 16:44:19 +0700

    upate api constant

--- src/apis/constants/index.ts
+++ src/apis/constants/index.ts
@@ -2,8 +2,8 @@ const REGISTER = '/api/v1/member/add';
 const LOGIN = 'api/v1/Auth/login';
 const SEARCH_MEMBER = '/api/v1/member/search';
 const GET_PROVINCE = '/api/v1/Address/ds-tinh';
-const GET_DISTRICT = '/api/v1/Address/ds-huyen/';
-const GET_VILLAGE = '/api/v1/Address/ds-xa/';
+const GET_DISTRICT = '/api/v1/Address/ds-huyen';
+const GET_VILLAGE = '/api/v1/Address/ds-xa';
 
 const API = {
   REGISTER,
