d57eafcda273d51ffcfa0e9b3c37a2dec75a9807
Huy Tuyen <fsoft.datnn@gmail.com>
2022-11-10 12:50:39 +0700

    update address api

--- src/apis/constants/index.ts
+++ src/apis/constants/index.ts
@@ -1,10 +1,11 @@
 const REGISTER = '/api/v1/member/add';
 const LOGIN = 'api/v1/Auth/login';
 const SEARCH_MEMBER = '/api/v1/member/search';
-const GET_PROVINCE = 'api/v1/Address/ds-tinh';
-const GET_DISTRICT = '/api/v1/Address/ds-huyen';
-const GET_VILLAGE = '/api/v1/Address/ds-xa';
+const GET_PROVINCE = '/api/address/Province';
+const GET_DISTRICT = '/api/address/District';
+const GET_VILLAGE = '/api/address/Ward';
 const GET_SKILL = '/api/v1/Skill/getall';
+const GET_CTN = '/api/ctn/list';
 
 const API = {
   REGISTER,
@@ -14,6 +15,7 @@ const API = {
   GET_DISTRICT,
   GET_VILLAGE,
   GET_SKILL,
+  GET_CTN,
 };
 
 export default API;
