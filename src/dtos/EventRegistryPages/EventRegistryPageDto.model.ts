392f4d50d7440dc0751eca2940c12f054509e0a1
Huy Tuyen <fsoft.datnn@gmail.com>
2022-11-12 12:33:06 +0700

    update dto

--- src/dtos/EventRegistryPages/EventRegistryPageDto.model.ts
+++ src/dtos/EventRegistryPages/EventRegistryPageDto.model.ts
@@ -2,14 +2,14 @@ import { ScopeType } from "../Enums/ScopeType.enum";
 import { DepartmentDto } from "../Departments/DepartmentDto.model";
 
 export type EventRegistryPageDto = {
-	eventId : number;
-	id : string | undefined;
-	name : string | undefined;
-	end : string;
-	start : string;
-	type : ScopeType;
-	ctnId : number | undefined;
-	pageContentId : number | undefined;
-	departments : DepartmentDto[] | undefined;
-	departmentIds : number[];
+	eventId?: number;
+	id?: string;
+	name?: string;
+	end?: string;
+	start?: string;
+	type?: ScopeType;
+	ctnId?: number;
+	pageContentId?: number;
+	departments?: DepartmentDto[];
+	departmentIds?: number[];
 }
