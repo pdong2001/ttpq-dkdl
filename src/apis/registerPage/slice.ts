cd5c42b73b8386055ef55705710f8938b1c6dd0f
Huy Tuyen <fsoft.datnn@gmail.com>
2022-11-12 12:58:56 +0700

    update dto for slice

--- src/apis/registerPage/slice.ts
+++ src/apis/registerPage/slice.ts
@@ -1,8 +1,9 @@
 import { ReduxState } from '~/apis/common/type';
+import { EventRegistryPageDto } from '~/dtos/EventRegistryPages/EventRegistryPageDto.model';
 import { createAsyncRequest } from '../common/action';
 import createAppSlice from '../common/slice';
 
-type RegisterPageState = ReduxState;
+type RegisterPageState = ReduxState<EventRegistryPageDto>;
 const initialState: RegisterPageState = {
   data: {},
 };
