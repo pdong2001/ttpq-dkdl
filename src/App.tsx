ed6e2edbfc7830684b0b94052bee0749d82d0362
Huy Tuyen <fsoft.datnn@gmail.com>
2022-11-21 14:25:06 +0700

    import global slick carousel css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Switch } from 'react-router-dom';
 import { ROUTES } from './routes';
 import { AppRoute } from './routes';
 import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
   return (
